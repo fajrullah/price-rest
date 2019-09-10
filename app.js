d97d9b977751b39eb9974705b3151413c92c62d7
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-10 16:13:39 +0700

    [API] Price Monitor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const  express = require(‘express’),
+const  express = require('express'),
  	   port = process.env.PORT || 4000,  
  	   app = express();
 
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’}));
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ status : 'Testing API Price Monitor'}));
 });
 
 app.listen(port, function () {
